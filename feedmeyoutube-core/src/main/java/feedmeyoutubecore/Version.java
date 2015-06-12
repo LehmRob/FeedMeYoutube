7fd9ddd3a845b9e0650d99a0a92bf3410ce6a305
Robert Lehmann <lehmrob@gmail.com>
2015-06-12 22:28:38 +0200

    Initial commit

---
+++ feedmeyoutube-core/src/main/java/feedmeyoutubecore/Version.java
@@ -0,0 +1,8 @@
+package feedmeyoutubecore;
+
+/**
+ * Created by rlehmann on 10.06.15.
+ */
+public class Version {
+    public static String VERSION = "1.0-Snapshot";
+}
---
+++ feedmeyoutube-crawler/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by rlehmann on 10.06.15.
+ */
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Hello WOlrd");
+    }
+}

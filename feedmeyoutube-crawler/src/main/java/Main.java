0ebefd1222bccdcf9bc34dcefe4318f0bec9db9e
Robert Lehmann <lehmrob@gmail.com>
2015-06-13 19:06:32 +0200

    crawler: Main: Move main into Package

--- feedmeyoutube-crawler/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by rlehmann on 10.06.15.
- */
-public class Main {
-    public static void main(String [] args) {
-        System.out.println("Hello WOlrd");
-    }
-}
---
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
@@ -0,0 +1,11 @@
+package feedmeyoutubecrawler;
+
+/**
+ * Created by rlehmann on 13.06.15.
+ */
+public class Main {
+    public static void main(String[] arg)
+    {
+        System.out.println("Hello World");
+    }
+}

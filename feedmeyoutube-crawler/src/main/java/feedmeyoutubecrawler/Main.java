6c60b13dd39185a57ac183cce2dc13416e808140
Robert Lehmann <lehmrob@gmail.com>
2015-06-28 20:27:56 +0200

    Crawler: Main: Add call for the crawler

--- feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
@@ -1,25 +1,14 @@
 package feedmeyoutubecrawler;
 
-// The MIT License (MIT)
-// Copyright (c) 2015 Robert Lehmann
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+/* Copyright (C) 2015 Robert Lehmann <lehmrob@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
 
 import feedmeyoutubecore.Version;
+import java.io.IOException;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,8 +24,12 @@ public class Main
     public static void main(String[] arg)
     {
         LOG.debug("Starting App in Version {}", Version.VERSION);
-        final Crawler crawler = new Crawler();
-
-        crawler.run();
+        final Crawler crawler;
+        try {
+            crawler = new Crawler();
+            System.out.println(crawler.getUploadedVideos());
+        } catch (final IOException ex) {
+            LOG.error("Error during the initialisation of the crawler", ex);
+        }
     }
 }

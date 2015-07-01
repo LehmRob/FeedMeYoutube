cd697d5143db92bb325b1b117a481f7c40166d5d
Robert Lehmann <lehmrob@gmail.com>
2015-07-01 19:07:33 +0200

    core: AppConfig: Initial commit
    The configuration of the hole application (crawler and core) is now implemented in the core. Thats the only configuration of the app.

---
+++ feedmeyoutube-core/src/main/java/feedmeyoutubecore/AppConfig.java
@@ -0,0 +1,41 @@
+/* Copyright (C) 2015 Robert Lehmann <lehmrob@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+package feedmeyoutubecore;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.Key;
+
+/**
+ * Interface for the configuration.
+ *
+ * @author Robert Lehmann
+ * @since 1.0
+ */
+@Config.Sources({"file:~/.crawler.config",
+    "classpath:crawler.config"})
+public interface AppConfig extends Config {
+    @Key("youtube.userId")
+    String userId();
+
+    @Key("youtube.channelId")
+    String channelId();
+
+    @Key("mysql.user")
+    String mysqlUser();
+    
+    @Key("mysql.password")
+    String mysqlPassword();
+    
+    @Key("mysql.host")
+    String mysqlHost();
+    
+    @Key("mysql.driver")
+    String mysqlDriver();
+    
+    @Key("mysql.db")
+    String mysqlDb();
+}

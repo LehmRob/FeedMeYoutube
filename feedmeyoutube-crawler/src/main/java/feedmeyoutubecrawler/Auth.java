19cdb8fd4db2fac1feb4523177d5cd6f2e097ed1
Robert Lehmann <lehmrob@gmail.com>
2015-07-01 19:12:13 +0200

    Auth: Change the config interface
    Use the config interface of the core because the configuration is now centralized

--- feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Auth.java
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Auth.java
@@ -5,7 +5,6 @@ package feedmeyoutubecrawler;
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
-
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.StoredCredential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -18,17 +17,14 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import feedmeyoutubecore.AppConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -38,42 +34,57 @@ import java.util.regex.Pattern;
  *
  * @since 1.0
  */
-public class Auth
-{
+public class Auth {
+
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
     private static final String CREDENTIALS_DIRECTORY = ".oauth-credentials";
-    private static final CrawlerConfig config = ConfigFactory.create(CrawlerConfig.class);
+    private static final AppConfig config = ConfigFactory.
+            create(AppConfig.class);
     private static final Pattern HOME_DIR = Pattern.compile("~");
     private static final Logger LOG = LoggerFactory.getLogger(Auth.class);
 
     /**
      * Perform the authorisation for the youtube account
-     * @param scopes {@linkplain List} of scopes to perform authorization
+     *
+     * @param scopes              {@linkplain List} of scopes to perform
+     *                            authorization
      * @param credentailDataStore name of the credential datastore
      *
      * @return {@linkplain Credential} object which is used for Requests
+     *
      * @throws IOException an error occurs during the authorisation.
      *
      * @since 1.0
      */
-    public static Credential authorize (List<String> scopes, String credentailDataStore)
+    public static Credential authorize(List<String> scopes,
+            String credentailDataStore)
             throws IOException {
-        final Reader reader = new InputStreamReader(Auth.class.getResourceAsStream("/youtube.json"));
-        final GoogleClientSecrets secrets = GoogleClientSecrets.load(JSON_FACTORY, reader);
-        final FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(Paths.get(System
-                .getProperty("user.home") + "/" + CREDENTIALS_DIRECTORY).toFile());
-        final DataStore<StoredCredential> dataStore = dataStoreFactory.getDataStore(credentailDataStore);
-        final GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-                secrets, scopes).setCredentialDataStore(dataStore).build();
-        final LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
+        final Reader reader = new InputStreamReader(Auth.class.
+                getResourceAsStream("/youtube.json"));
+        final GoogleClientSecrets secrets = GoogleClientSecrets.load(
+                JSON_FACTORY, reader);
+        final FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(
+                Paths.get(System
+                        .getProperty("user.home") + "/" + CREDENTIALS_DIRECTORY).
+                toFile());
+        final DataStore<StoredCredential> dataStore = dataStoreFactory.
+                getDataStore(credentailDataStore);
+        final GoogleAuthorizationCodeFlow flow
+                = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT,
+                        JSON_FACTORY,
+                        secrets, scopes).setCredentialDataStore(dataStore).
+                build();
+        final LocalServerReceiver receiver = new LocalServerReceiver.Builder().
+                setPort(8080).build();
 
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize(config.userId());
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize(
+                config.userId());
     }
-
 
     /**
      * Gets the instance of {@linkplain HttpTransport}
+     *
      * @return instance of {@linkplain HttpTransport}
      *
      * @since 1.0
@@ -84,6 +95,7 @@ public class Auth
 
     /**
      * Gets the instance of {@linkplain JsonFactory}
+     *
      * @return instance of {@linkplain JsonFactory}
      *
      * @since 1.0

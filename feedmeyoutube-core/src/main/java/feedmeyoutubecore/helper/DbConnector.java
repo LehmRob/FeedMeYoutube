07c1e800f78901d014cd4eab6fa78af5dd329f88
Robert Lehmann <lehmrob@gmail.com>
2015-07-01 19:08:36 +0200

    DbConnector: Read config from config file.
    The parameters for the database connection are now readed from the config file.

--- feedmeyoutube-core/src/main/java/feedmeyoutubecore/helper/DbConnector.java
+++ feedmeyoutube-core/src/main/java/feedmeyoutubecore/helper/DbConnector.java
@@ -1,22 +1,25 @@
 package feedmeyoutubecore.helper;
 
+import feedmeyoutubecore.AppConfig;
 import feedmeyoutubecore.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import org.aeonbits.owner.ConfigFactory;
 
 public class DbConnector {
     // The Logger
     private static final Logger Log = LoggerFactory.getLogger(Main.class);
+    private static final AppConfig CONFIG = ConfigFactory.create(AppConfig.class);
 
     // Configuration
-    private static String dbHost = "localhost";
-    private static String dbName = "webseite";
-    private static String dbDriver = "com.mysql.jdbc.Driver";
-    private static String dbUser = "YtbUser";
-    private static String dbPass = "12345"; // Only for testing, for god's sake, secure your Database better than me :O
+    private static final String dbHost = CONFIG.mysqlHost();
+    private static final String dbName = CONFIG.mysqlDb();
+    private static final String dbDriver = CONFIG.mysqlDriver();
+    private static final String dbUser = CONFIG.mysqlUser();
+    private static final String dbPass = CONFIG.mysqlPassword();
 
     public static Connection Connect()
     {

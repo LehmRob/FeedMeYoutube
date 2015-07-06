7c8aaf7a3fc1ddb8983ac20870d51bdb1f78c9e1
Robert Lehmann <lehmrob@gmail.com>
2015-07-06 20:49:06 +0200

    crawler: Main: Remove unused import

--- feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
@@ -8,7 +8,6 @@ package feedmeyoutubecrawler;
 
 import feedmeyoutubecore.Version;
 import java.io.IOException;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

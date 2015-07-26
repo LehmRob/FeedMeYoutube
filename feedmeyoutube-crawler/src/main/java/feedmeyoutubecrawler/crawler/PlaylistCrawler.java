72004703fbb800c6dd7da362490a395018d03801
Robert Lehmann <lehmrob@gmail.com>
2015-07-26 19:58:49 +0200

    PlaylistCrawler: Intial commit
    Add the harness of the class.

---
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/crawler/PlaylistCrawler.java
@@ -0,0 +1,17 @@
+/* Copyright (C) 2015 Robert Lehmann <lehmrob@gmail.com>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+package feedmeyoutubecrawler.crawler;
+
+/**
+ * Crawl all youtube videos for the configured channel. Crawling all classes
+ * with the desired video ids.
+ * 
+ * @author Robert Lehmann
+ * @since 1.0
+ */
+public class PlaylistCrawler {
+    
+}

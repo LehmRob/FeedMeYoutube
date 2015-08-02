c0659a032619e02236bf66e77c049bda48b6aa8c
Robert Lehmann <lehmrob@gmail.com>
2015-08-02 20:19:46 +0200

    PlaylistCrawler: Add constructor
    The constructor takes the youtube connection and creates the own channel
    request.

--- feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/crawler/PlaylistCrawler.java
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/crawler/PlaylistCrawler.java
@@ -5,6 +5,9 @@
  */
 package feedmeyoutubecrawler.crawler;
 
+import com.google.api.services.youtube.model.Channel;
+import java.io.IOException;
+
 /**
  * Crawl all youtube videos for the configured channel. Crawling all classes
  * with the desired video ids.
@@ -13,5 +16,19 @@ package feedmeyoutubecrawler.crawler;
  * @since 1.0
  */
 public class PlaylistCrawler {
+    private final YouTubeConnection _connection;
+    private final Channel _channel;
     
+    /**
+     * Constructor. Initialize the playlist crawler.
+     * @param connection instance for {@link YouTubeConnection}.
+     * @throws IOException Error while requesting the own {@link Channel}
+     * refence
+     * @since 1.0
+     */
+    public PlaylistCrawler(final YouTubeConnection connection) throws 
+            IOException {
+        _connection = connection;
+        _channel = CrawlerUtils.getMyChannel(connection);
+    }
 }

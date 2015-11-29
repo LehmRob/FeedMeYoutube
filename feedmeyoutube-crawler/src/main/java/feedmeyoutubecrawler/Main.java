6dcb610bd02df8b587765dcfdf9297739bd04e45
Robert Lehmann <lehmrob@gmail.com>
2015-11-29 13:29:26 +0100

    crawler: Remove usage of Crawler class
    Remove the usage of the crawler class and use the Videocrawler directly

--- feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
+++ feedmeyoutube-crawler/src/main/java/feedmeyoutubecrawler/Main.java
@@ -6,11 +6,15 @@ package feedmeyoutubecrawler;
  * of the MIT license.  See the LICENSE file for details.
  */
 
-import feedmeyoutubecrawler.crawler.Crawler;
 import feedmeyoutubecore.Version;
-import java.io.IOException;
+import feedmeyoutubecore.obj.YouTubeVideo;
+import feedmeyoutubecrawler.crawler.VideoCrawler;
+import feedmeyoutubecrawler.crawler.YouTubeConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Main class which runs the main application.
@@ -24,12 +28,16 @@ public class Main
     public static void main(String[] arg)
     {
         LOG.debug("Starting App in Version {}", Version.VERSION);
-        final Crawler crawler;
+
         try {
-            crawler = new Crawler();
-            crawler.crawl();
-        } catch (final IOException ex) {
-            LOG.error("Error during the initialisation of the crawler", ex);
+            final VideoCrawler vc = new VideoCrawler(new YouTubeConnection());
+            if (vc.hasNext()) {
+                List<YouTubeVideo> videos = vc.getNextVideos();
+                LOG.info("Size of video list {}", videos.size());
+                videos.stream().forEach(video -> LOG.info("Video info {}", video.VideoId));
+            }
+        } catch (final IOException e) {
+            LOG.error("Can't connect to youtube", e);
         }
     }
 }
